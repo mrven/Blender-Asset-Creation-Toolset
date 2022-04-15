58a85a6083b5e78e1229216043d3c2fb449b275a
Ivan Vostrikov <ivan.vostrikov@tinybuild.com>
2022-04-15 21:37:59 +0300

    [New] OBJ Export
    Small Refactoring

--- Asset_Creation_Toolset_3_1_6_310/utils.py
+++ Asset_Creation_Toolset_3_1_6_310/utils.py
@@ -53,3 +53,37 @@ def Get_Version():
 		result = 2.90
 	
 	return result
+
+#-------------------------------------------------------
+# Export Model
+def export_model(path, name, fbx_scale_mode):
+	act = bpy.context.scene.act
+	if act.export_custom_options:
+		if act.export_format == 'FBX':
+			bpy.ops.export_scene.fbx(filepath=str(path + name + '.fbx'), use_selection=True,
+									apply_scale_options=fbx_scale_mode,
+									use_mesh_modifiers=True, mesh_smooth_type=act.export_smoothing,
+									use_mesh_edges=act.export_loose_edges, use_tspace=act.export_tangent_space)
+		if act.export_format == 'OBJ':
+			bpy.ops.export_scene.obj(filepath=str(path + name + '.obj'), use_selection=True, use_mesh_modifiers=True,
+									use_edges=True, use_smooth_groups=act.obj_export_smooth_groups,
+									use_normals=True, use_uvs=True, use_materials=True,
+									use_triangles=act.triangulate_before_export, group_by_object=True,
+									group_by_material=act.obj_separate_by_materials,
+									keep_vertex_order=True, global_scale=1, path_mode='AUTO',
+									axis_forward='-Z', axis_up='Y')
+	else:
+		if act.export_format == 'FBX':
+			if act.export_target_engine == 'UNITY':
+				bpy.ops.export_scene.fbx(filepath=str(path + name + '.fbx'), use_selection=True,
+										apply_scale_options='FBX_SCALE_ALL')
+			else:
+				bpy.ops.export_scene.fbx(filepath=str(path + name + '.fbx'), use_selection=True,
+										apply_scale_options='FBX_SCALE_NONE', mesh_smooth_type='FACE', use_tspace=True)
+		if act.export_format == 'OBJ':
+			bpy.ops.export_scene.obj(filepath=str(path + name + '.obj'), use_selection=True, use_mesh_modifiers=True,
+									use_edges=True, use_smooth_groups=True, use_normals=True, use_uvs=True,
+									use_materials=True, use_triangles=act.triangulate_before_export,
+									group_by_object=True, group_by_material=True, keep_vertex_order=True,
+									global_scale=1, path_mode='AUTO', axis_forward='-Z', axis_up='Y')
+

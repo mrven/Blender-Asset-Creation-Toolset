732865d8885cef0e18df8db76b982baace09a7a6
Ivan Vostrikov <mrven@mail.ru>
2025-08-07 09:55:24 +0300

    reorganize repo

---
+++ archive/source/ACT_2025_1_Bl420/__init__.py
@@ -0,0 +1,43 @@
+bl_info = {
+	"name": "ACT: Game Asset Creation Toolset",
+	"description": "Tools for easy create and export low-poly game assets",
+	"author": "Ivan 'mrven' Vostrikov,  Felipe Torrents, mokalux, Oxicid, ani-kun",
+	"wiki_url": "https://github.com/mrven/Blender-Asset-Creation-Toolset#readme",
+	"tracker_url": "https://github.com/mrven/Blender-Asset-Creation-Toolset/issues",
+	"doc_url": "https://github.com/mrven/Blender-Asset-Creation-Toolset#readme",
+	"version": (2025, 1, 1),
+	"blender": (4, 2, 0),
+	"location": "3D View > Toolbox > ACT",
+	"category": "Object",
+}
+
+from . import props
+from . import preferences
+from . import utils
+from . import origin_tools
+from . import rename_tools
+from . import uv_tools
+from . import import_export_tools
+from . import material_tools
+from . import other_tools
+from . import geometry_tools
+
+modules_names = (
+	props,
+	origin_tools,
+	rename_tools,
+	uv_tools,
+	import_export_tools,
+	material_tools,
+	other_tools,
+	geometry_tools,
+	preferences
+)
+
+def register():
+	for module_name in modules_names:
+		module_name.register()
+
+def unregister():
+	for module_name in reversed(modules_names):
+		module_name.unregister()

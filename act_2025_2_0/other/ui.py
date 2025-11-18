b41ebf21ab972d9dd370536964e3953428144d3e
Ivan Vostrikov <mrven@mail.ru>
2025-11-18 23:07:28 +0300

    fix panel for armature edit and weight paint

--- act_2025_2_0/other/ui.py
+++ act_2025_2_0/other/ui.py
@@ -16,7 +16,7 @@ class VIEW3D_PT_other_tools_panel(bpy.types.Panel):
 	def poll(cls, context):
 		prefs = context.preferences.addons[package_name].preferences
 		return (context.object is not None and context.active_object is not None
-		        and context.object.mode in {"OBJECT", "EDIT_ARMATURE", "PAINT_WEIGHT"} and prefs.other_enable)
+		        and context.mode in {"OBJECT", "EDIT_ARMATURE", "PAINT_WEIGHT"} and prefs.other_enable)
 
 	def draw(self, context):
 		act = context.scene.act

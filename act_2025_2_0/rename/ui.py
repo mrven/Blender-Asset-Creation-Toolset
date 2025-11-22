c5bc638329b830517f4331c82360c71300d54fb4
Ivan Vostrikov <mrven@mail.ru>
2025-11-22 23:09:56 +0300

    fix panel class names

--- act_2025_2_0/rename/ui.py
+++ act_2025_2_0/rename/ui.py
@@ -5,7 +5,7 @@ from ..common import utils as common_utils
 
 package_name = common_utils.get_package_name()
 
-class VIEW3D_PT_rename_tools_panel(bpy.types.Panel):
+class VIEW3D_PT_act_rename_tools_panel(bpy.types.Panel):
 	bl_label = "Renaming Tools"
 	bl_space_type = "VIEW_3D"
 	bl_region_type = "UI"
@@ -51,7 +51,7 @@ class VIEW3D_PT_rename_tools_panel(bpy.types.Panel):
 
 
 classes = (
-	VIEW3D_PT_rename_tools_panel,
+	VIEW3D_PT_act_rename_tools_panel,
 )
 
 

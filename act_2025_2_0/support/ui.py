7e5c81a05e10095415c60721879a05628eb54e69
Ivan Vostrikov <mrven@mail.ru>
2025-11-23 10:46:14 +0300

    add support panel to uv editor; fix geometry panel category

--- act_2025_2_0/support/ui.py
+++ act_2025_2_0/support/ui.py
@@ -16,11 +16,30 @@ class VIEW3D_PT_act_support_panel(bpy.types.Panel):
 	def poll(cls, context):
 		prefs = context.preferences.addons[package_name].preferences
 		return (context.object is not None and context.active_object is not None
-		        and context.mode in {"OBJECT", "EDIT_MESH", "EDIT_ARMATURE", "PAINT_WEIGHT"} and prefs.support_enable)
+		        and context.mode in {"OBJECT", "EDIT_MESH", "EDIT_ARMATURE", "PAINT_WEIGHT"} and prefs.view3d_support_enable)
 
-	def draw(self, context):
-		act = context.scene.act
+	def draw(self, _):
+		layout = self.layout
 
+		box = layout.box()
+		row = box.row(align=True)
+		row.operator(operators.ShowAddonPrefs.bl_idname, text="Prefs", icon='PREFERENCES')
+		row.operator(operators.OpenURL.bl_idname, text="Docs", icon='HELP').url = constants.ACT_DOC_URL
+		row.operator(operators.OpenURL.bl_idname, text="Report", icon='URL').url = constants.ACT_REPORT_URL
+
+
+class UV_PT_act_support_panel(bpy.types.Panel):
+	bl_label = "ACT 2025.2 Support"
+	bl_space_type = "IMAGE_EDITOR"
+	bl_region_type = "UI"
+	bl_category = "ACT"
+
+	@classmethod
+	def poll(cls, context):
+		prefs = context.preferences.addons[package_name].preferences
+		return context.mode in {"OBJECT", "EDIT_MESH"} and prefs.uv_support_enable
+
+	def draw(self, _):
 		layout = self.layout
 
 		box = layout.box()
@@ -32,6 +51,7 @@ class VIEW3D_PT_act_support_panel(bpy.types.Panel):
 
 classes = (
 	VIEW3D_PT_act_support_panel,
+	UV_PT_act_support_panel,
 )
 
 
